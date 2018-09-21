fd3b1a204ec38a967dbe49295fa93f3873345bea
AI2EJAY <JHammett1314@gmail.com>
2018-09-21 11:27:28 -0500

    Routes wrote out and some imports made.
    Testing why they are not currently working

--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,77 @@
-const axios = require('axios');
-
-const { authenticate } = require('./middlewares');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const { authenticate } = require("./middlewares");
+const db = require("../database/dbConfig");
+const jwt = require("jsonwebtoken");
+const secret = require("../_secrets/keys");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "10d",
+    jwtid: "55566"
+  };
+  return jwt.sign(payload, secret, options);
 }
+
+function register(req, res) {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+
+      db("users")
+        .where({ id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          console.log(user);
+          res.status(201).json({ id: user.id, token });
+        })
+        .catch(err => res.status(500).json({ err1: err }));
+    })
+    .catch(err => res.status(500).json({ err2: err }));
+}
+// implement user registration
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch(err => res.status(500).send(err));
 }
 
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
